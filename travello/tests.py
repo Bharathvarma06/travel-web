c3db6601205528ab4d306acb7de87ec6ae121026
Bharathvarma06 <153293056+Bharathvarma06@users.noreply.github.com>
2025-03-28 11:19:51 +0530

    Update tests.py

--- travello/tests.py
+++ travello/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
 
 
-DJANGO_SETTINGS_MODULE = travel-web.settings
-python_files = tests.py test_*.py
+
 
 
 # Create your tests here.
+
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def test_basic_math(self):
+        self.assertEqual(1 + 1, 2)  # Example test
+
